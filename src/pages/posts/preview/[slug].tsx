8815f7421391f0df72a54b9b428b3625510dd9c6
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-11 14:40:45 -0300

    Finalizando

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,20 +1,20 @@
-import { GetStaticProps } from 'next'
-import {  useSession } from 'next-auth/client'
-import Head from 'next/head'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { RichText } from 'prismic-dom'
-import { useEffect } from 'react'
-import { getPrismicClient } from '../../../services/prismic'
+import { GetStaticPaths, GetStaticProps } from "next"
+import { getSession, useSession } from "next-auth/client"
+import Head from "next/head"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { RichText } from "prismic-dom"
+import { useEffect } from "react"
+import { getPrismicClient } from "../../../services/prismic"
 
 import styles from '../post.module.scss'
 
 interface PostPreviewProps {
   post: {
-    slug: string
-    title: string
-    content: string
-    updatedAt: string
+    slug:string;
+    title:string;
+    content:string;
+    updatedAt:string;
   }
 }
 
@@ -38,6 +38,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
         <article className={styles.post}>
           <h1>{post.title}</h1>
           <time>{post.updatedAt}</time>
+
           <div
             className={`${styles.postContent} ${styles.previewContent}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
@@ -55,8 +56,16 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params
+  const { slug } = params;
+
   const prismic = getPrismicClient()
 
   const response = await prismic.getByUID('publication', String(slug), {})
@@ -65,19 +74,16 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content.splice(0, 3)),
-    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
-      'pt-BR',
-      {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-      }
-    ),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
   }
 
   return {
     props: {
-      post,
+      post
     },
     revalidate: 60 * 30, // 30 minutes
   }
