847d967c885320178838f2108b8632d7ea0a63d9
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-11 14:35:33 -0300

    Finalizando

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,5 +1,5 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
-import { getSession, useSession } from 'next-auth/client'
+import { GetStaticProps } from 'next'
+import {  useSession } from 'next-auth/client'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
