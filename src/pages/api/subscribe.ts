17f9ab191d011fed3cfda550977150d82d265fb4
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-11 14:58:08 -0300

    Finalizado

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,11 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
 import { stripe } from '../../services/stripe';
+import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { query as q } from 'faunadb'
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST') {
       const session = await getSession({ req })
+
+      const user = await fauna.query<User>(
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(session.user.email)
+          )
+        )
+      )
+  
+      let customerID = user.data.stripe_customer_id
+  
+      if(!customerID) {
+        const stripeCustomer = await stripe.customers.create({
+          email: session.user.email,
+          // metadata
+        })
+  
+        await fauna.query(
+          q.Update(
+            q.Ref(q.Collection('users'), user.ref.id),
+            {
+              data: {
+                stripe_customer_id: stripeCustomer.id
+              }
+            }
+          )
+        )
+        
+        customerID = stripeCustomer.id
+      }
 
       const stripeCustomer = await stripe.customers.create({
           email: session.user.email,
